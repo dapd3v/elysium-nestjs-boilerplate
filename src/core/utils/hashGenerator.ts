9ba72e88876c826d7cc8c462f4026c897f949719
Byo-xiao <naxterpic@gmail.com>
2024-09-20 13:31:58 -0500

    ✨ feat(utils): Add hashGenerator function
    Implemented hashGenerator utility for secure hashing
    Enhanced security for sensitive data by using the hash generator function in relevant modules

---
+++ src/core/utils/hashGenerator.ts
@@ -0,0 +1,14 @@
+import crypto from 'crypto';
+import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
+
+export const hashGenerator = () => {
+
+  const randomString = randomStringGenerator();
+
+  const hash = crypto
+      .createHash('sha256')
+      .update(randomString)
+      .digest('hex');
+      
+  return hash;
+};
